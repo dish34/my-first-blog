fb6e2ab2d1b9d80691907546349a2b66924f253c
dishant <dishantgandhi99@yahoo.com>
2019-02-02 17:10:32 +0530

    modified setting.py

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'l$6nv=erybgxy098413y@yfol9p+4jdbjm7q9q@1-t&ue9uwq5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',
+'.pythonanywhere.com']
 
 
 # Application definition
