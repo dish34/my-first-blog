87696817d2ca6f65c8055af9f2c844fdd8b2a424
dishant <dishantgandhi99@yahoo.com>
2019-02-10 12:29:35 +0530

    added post_list_draft.html

--- blog/views.py
+++ blog/views.py
@@ -17,7 +17,7 @@ def post_new(request):
     if form.is_valid():
         post = form.save(commit=False)
         post.author = request.user
-        post.published_date = timezone.now()
+        #post.published_date = timezone.now()
         post.save()
         return redirect('post_detail', pk=post.pk)
     else:
@@ -31,9 +31,22 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            #post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_draft_list(request):
+     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+     return render(request,'blog/post_draft_list.html',{'posts':posts})
+
+def post_publish(request,pk):
+      post = get_object_or_404(post, pk=pk)
+      post.publish()
+      return redirect('post_detail',pk=pk)
+
+def publish(self):
+      self.published_date = timezone.now()
+      self.save()
